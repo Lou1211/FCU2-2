cc1eb285e995155427daf524cc5f337c47016a4c
Lou1211 <cindy20021211@gmail.com>
2022-05-13 14:49:35 +0800

    1

--- 0509/Q.java
+++ 0509/Q.java
@@ -1,7 +1,7 @@
 public class Q {
     public static void main(String[] args)
     {
-        Q3 win = new Q3();
-       win.setVisible(true);
+        E cp = new E();
+       cp.setVisible(true);
     }
 }
